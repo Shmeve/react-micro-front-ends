811e15607515f7572296a1635932a6565aeee747
Steve Ferreira <steve@Steves-MacBook-Air.local>
2021-06-13 12:24:59 -0400

    Intro to execution contexts; running isolated vs container

--- ecomm/cart/src/bootstrap.js
+++ ecomm/cart/src/bootstrap.js
@@ -1,5 +1,22 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+const mount = (el) => {
+    const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+    el.innerHTML = cartText;
+}
+
+// Execution Context 1: Running in dev (isolation) -> Local index.html
+// Immediate render in local element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#dev-cart");
+
+    if (el) {
+        mount(el);
+    }
+}
+
+// Execution Context 2: Running in dev/prod (within Container) -> Container/index.html
+// Wait until called to render in remote element
+export { mount };
--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,12 +1,27 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+    let products = '';
 
-for (let i = 0; i < 5; i++) {
-    const name = faker.commerce.productName();
+    for (let i = 0; i < 5; i++) {
+        const name = faker.commerce.productName();
 
-    products += `<div>${name}</div>`;
+        products += `<div>${name}</div>`;
+    }
+
+    el.innerHTML = products;
 }
 
-//console.log(products);
-document.querySelector("#dev-products").innerHTML = products;
+// Execution Context 1: Running in dev (isolation) -> Local index.html
+// Immediate render in local element
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector("#dev-products");
+
+    if (el) {
+        mount(el);
+    }
+}
+
+// Execution Context 2: Running in dev/prod (within Container) -> Container/index.html
+// Wait until called to render in remote element
+export { mount };
