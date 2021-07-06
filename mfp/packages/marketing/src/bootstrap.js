7201ded02d1fecab5eb425675100d807bfe3f1e3
Steve Ferreira <sjferreira516@gamil.com>
2021-07-06 13:34:49 -0400

    Implement multi-tier history navigation across apps

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+    const history = createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (nextPathname !== pathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 }
 
 if (process.env.NODE_ENV === 'development') {
     const el = document.querySelector('#_marketing-dev-root');
 
     if (el) {
-        mount(el);
+        mount(el, {});
     }
 }
 
