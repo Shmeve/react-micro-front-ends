de9d248b05c7cc04c941092be48832998b19e81b
Steve Ferreira <sjferreira516@gamil.com>
2021-07-06 19:55:31 -0400

    Add minimal mock login functionality

--- mfp/packages/container/src/components/AuthApp.js
+++ mfp/packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
     const ref = useRef(null);
     const history = useHistory();
 
@@ -15,7 +15,11 @@ export default () => {
                 if (nextPathname !== pathname) {
                     history.push(nextPathname);
                 }
-            }
+            },
+            onSignIn
+            // onSignIn: () => {
+            //     onSignIn;
+            // }
         });
 
         history.listen(onParentNavigate);
